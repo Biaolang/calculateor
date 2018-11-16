7de9d19784b4712e16a81b778de782cfdae35bda
libin0205 <1574747737@qq.com>
2018-11-16 21:10:25 +0800

    Update calculateor/ViewController.swift

--- calculateor/ViewController.swift
+++ calculateor/ViewController.swift
@@ -7,8 +7,41 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+class stack1{
+    var top:Int = 0
+    var flag:Int = 0
+    var num = [Int](repeating: 0, count: 100)
+    
+    func PushStack(x:Int) -> Int {
+        num[top] = x;
+        top = top+1
+        return 1;
+    }
+    func PopStack() -> Int {
+        var f = num[top]
+        f = f-1
+        return f
+    }
+    
+    
+};
+class stack2{
+    var top:Int = 0
+    var flag:Int = 0
+ var num = [String](repeatElement("123", count: 100))
+    func PushStack(x:String) -> Int {
+        num[top] = x;
+        top = top+1
+        return 1;
+    }
+    func PopStack() -> String {
+        var f = num[top]
+        top = top-1
+        return f
+    }
+    
+    
+};class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +51,13 @@ class ViewController: UIViewController {
 
 }
 
+class MarksStruct {
+    var mark: Int
+    init(mark: Int) {
+        self.mark = mark
+    }
+}
+
+class studentMarks {
+    var mark = 300
+}
